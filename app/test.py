23e9a162bcb4fbc385b6354fb6130e45454019d0
SantoshSR14 <sec23cs175@sairamtap.edu.in>
2026-02-04 21:54:32 +0530

    Delete app/test.py

--- app/test.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyANiB51tLqSirWfQ_mONdBOoh_4y3e0Rbg")
-
-model = genai.GenerativeModel("gemini-2.5-flash")
-response = model.generate_content("Say hello like a human")
-
-print(response.text)
