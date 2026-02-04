53d451112f9122c2eb1fa37c269feb2deb9a707e
SantoshSR14 <sec23cs175@sairamtap.edu.in>
2026-02-04 21:34:59 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-API_KEY = os.getenv("HONEYPOT_API_KEY")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-
-GUVI_CALLBACK_URL = "https://hackathon.guvi.in/api/updateHoneyPotFinalResult"
